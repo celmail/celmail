bc56060b044da53340fa2475497c72d85f3ff47c
john bahamon <johnfredybahamon@gmail.com>
2021-03-13 12:04:58 -0500

    Iniciando el proyecto

---
+++ index.js
@@ -0,0 +1,18 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+const parser = require("mailparser").simpleParser
+
+const server = new SMTPServer({
+  onData(stream, session, callback) {
+    parser(stream, {}, (err, parsed) => {
+      if (err)
+        console.log("Error:" , err)
+      
+      console.log(parsed)
+      stream.on("end", callback)
+    })
+    
+  },
+  disabledCommands: ['AUTH']
+});
+
+server.listen(25, "192.168.30.100")
