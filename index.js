60c4e21942ff6036eae1222a08db8652df37dacf
john bahamon <johnfredybahamon@gmail.com>
2021-03-13 12:14:10 -0500

    instalacion express

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
 const SMTPServer = require("smtp-server").SMTPServer;
 const parser = require("mailparser").simpleParser
 
-const server = new SMTPServer({
-  onData(stream, session, callback) {
-    parser(stream, {}, (err, parsed) => {
-      if (err)
-        console.log("Error:" , err)
-      
-      console.log(parsed)
-      stream.on("end", callback)
-    })
-    
-  },
-  disabledCommands: ['AUTH']
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 });
+app.get('/server', (req, res) => {
 
-server.listen(25, "192.168.30.100")
+    const server = new SMTPServer({
+    onData(stream, session, callback) {
+      parser(stream, {}, (err, parsed) => {
+        if (err)
+          console.log("Error:" , err)
+        
+        console.log(parsed)
+        stream.on("end", callback)
+      })
+      
+    },
+    disabledCommands: ['AUTH']
+    });
+  
+    server.listen(25, "192.168.30.100")
+    res.send('Hello World!')
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
