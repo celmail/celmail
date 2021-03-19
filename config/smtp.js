86e98fb11358a94b36e0b35260bda5cd100bf1c7
John Bahamon <johnfredybahamon@gmail.com>
2021-03-18 23:41:49 -0500

    testeando datos

--- config/smtp.js
+++ config/smtp.js
@@ -1,15 +1,23 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 const parser = require("mailparser").simpleParser;
+var fs = require('fs');
+
 
 module.exports = function() {
     const server = new SMTPServer({
         onData(stream, session, callback) {
-          parser(stream, {}, (err, parsed) => {
-            if (err)
-              console.log("Error:" , err)
-            
-            console.log(parsed)
-            stream.on("end", callback)
+            parser(stream, {}, (err, parsed) => {
+                if (err)
+                    console.log("Error:" , err)
+                console.log(parsed)
+                fs.writeFileSync('mail.json', JSON.stringify(parsed), (err) => {
+                    // throws an error, you could also catch it here
+                    if (err) throw err;
+                
+                    // success case, the file was saved
+                    console.log('mail saved!');
+                });
+                stream.on("end", callback)
           })
         },
         disabledCommands: ['AUTH']
